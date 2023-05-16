ee6f347b5f0043ac4f4ae630422c0417e6e51e90
Rumbustious <99288580+Rumbustious@users.noreply.github.com>
2023-05-16 22:12:34 +0300

    Displaying basic dynamic 2D matrix

--- addition.cpp
+++ addition.cpp
@@ -3,6 +3,29 @@
 
 using namespace std;
 int main () {
-   
+    int rows, cols, num;
+    cin >> rows >> cols;
+    // Initializing the vector of vectors
+    vector<vector<int> > vec;
+    
+    for (int i = 0; i < rows; i++) {
+        // Vector to store column elements
+        vector<int> v1;
+  
+        for (int j = 0; j < cols; j++) {
+            cin >> num;
+            v1.push_back(num);
+        }
+        // Pushing back above 1D vector
+        // to create the 2D vector
+        vec.push_back(v1);
+    }
+
+    // Displaying the 2D vector
+    for (int i = 0; i < vec.size(); i++) {
+        for (int j = 0; j < vec[i].size(); j++)
+            cout << vec[i][j] << " ";
+        cout << endl;
+    }
     return 0;
 }
