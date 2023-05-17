fff0fc648932008df92bf84fb11405ed24810875
Rumbustious <99288580+Rumbustious@users.noreply.github.com>
2023-05-17 23:30:50 +0300

    Created seperate files for function     - Refactored some output style

---
+++ addition.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+void addition(vector<vector<int>> vec1, vector<vector<int>> vec2){
+
+for (int i = 0; i < vec1.size(); i++) {
+        for (int j = 0; j < vec2[i].size(); j++)
+            cout << vec2[i][j] + vec1[i][j] << " ";
+        cout << endl;
+    }
+}
