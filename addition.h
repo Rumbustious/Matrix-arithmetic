c7917e78330beb91d7937b1579c991f7dd59d47c
mkhmkhmkh123 <mkhmkhmkh123@gmail.com>
2023-05-17 23:39:11 +0300

    initial commit link header file addition.h with the main.cpp file

---
+++ addition.h
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+void add(vector<vector<int>> vec1, vector<vector<int>> vec2){
+
+for (int i = 0; i < vec1.size(); i++) {
+        for (int j = 0; j < vec2[i].size(); j++)
+            cout << vec2[i][j] + vec1[i][j] << " ";
+        cout << endl;
+    }
+
+}
--- main.cpp
+++ main.cpp
@@ -1,17 +1,8 @@
 #include<iostream>
 #include<vector>
+#include"addition.h"
 
 using namespace std;
-
-void add(vector<vector<int>> vec1, vector<vector<int>> vec2){
-
-for (int i = 0; i < vec1.size(); i++) {
-        for (int j = 0; j < vec2[i].size(); j++)
-            cout << vec2[i][j] + vec1[i][j] << " ";
-        cout << endl;
-    }
-
-}
 
 void sub(vector<vector<int>> vec1, vector<vector<int>> vec2){
 
