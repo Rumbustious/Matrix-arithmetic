68fe6e0d4d78b35fd71db02ae8149101932d3279
mkhmkhmkh123 <mkhmkhmkh123@gmail.com>
2023-05-18 03:42:09 +0300

    intial commit

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+for (int i = 0; i < temp.size(); i++){
+    //     for(int j = 0; j < temp[0].size(); j++){
+            
+    //         cout << temp[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+    // cout << endl;
