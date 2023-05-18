02c357e45493290efca54d1df13f736437197252
Rumbustious <99288580+Rumbustious@users.noreply.github.com>
2023-05-18 04:15:20 +0300

    added adjoint file

---
+++ cofactor.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <header/determinant.h>
+
+using namespace std;
+void cofactor(vector< vector<int> > matrix) {
+   
+        for (int i = 0; i < matrix.size(); i++){
+            for (int j = 0; j < matrix.size(); j++) {
+                matrix[i][j];
+            }
+        }
+}
+int main () {
+    return 0;
+}
