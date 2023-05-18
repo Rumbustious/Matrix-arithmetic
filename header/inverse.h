00b78f354caf032415c2730995af074da1c14028
mkhmkhmkh123 <mkhmkhmkh123@gmail.com>
2023-05-18 08:03:01 +0300

    Add inverse.h

--- header/inverse.h
+++ header/inverse.h
@@ -5,7 +5,11 @@
 using namespace std;
 
 void inverse(vector<vector<int>> v1, int determinante){
-
+ for (int i = 0; i < v1.size(); i++){
+        for (int j = 0; j < v1.size(); j++){
+            cout << (v1[i][j]*(1.0/determinante)) << " ";        
+        }
+        cout << endl;
    
     }
 }
