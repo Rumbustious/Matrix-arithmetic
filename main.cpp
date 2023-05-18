d07a1ccba57e53ade7b108d0b2a02d3caba9660a
mkhmkhmkh123 <mkhmkhmkh123@gmail.com>
2023-05-18 08:23:51 +0300

    final commit

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include<iostream>
 #include<vector>
-#include "header/addition.h"
-#include "header/subtraction.h"
-#include "header/multiplication.h"
-#include "header/determinant.h"
+#include <addition.h>
 
 using namespace std;
+
+void addition(vector<vector<int>> vec1, vector<vector<int>> vec2);
 
 
 
@@ -63,29 +62,29 @@ int main(){
 
         if ((vec.size() != vec2.size()) || (vec[0].size() != vec2[0].size())){
              cout << "please enter two matrices with the same dimentions, Ex: two 4x4 matrices" << endl;
-            return 1;
-        }
-
-        add(vec,vec2);
-    }
-    else if (operation == 'S'){
-
-        if ((vec.size() != vec2.size()) || (vec[0].size() != vec2[0].size())){
-             cout << "please enter two matrices with the same dimentions, Ex: two 4x4 matrices" << endl;
-            return 1;
-        }
-
-        subtract(vec,vec2);
-    }
-    else if (operation == 'M'){
-
-        if (vec[0].size() != vec2.size()){
-             cout << "please enter two matrices with the compatible dimentions, The number of columns in A should equal the number of rows of B" << endl;
         return 1 ;
         }
-        // 2x3 . 3xY
-        multiply(vec,vec2);
+
+        addition(vec,vec2);
     }
+    // else if (operation == 'S'){
+
+    //     if ((vec.size() != vec2.size()) || (vec[0].size() != vec2[0].size())){
+    //          cout << "please enter two matrices with the same dimentions, Ex: two 4x4 matrices" << endl;
+    //     return 1 ;
+    //     }
+
+    //     sub(vec,vec2);
+    // }
+    // else if (operation == 'M'){
+
+    //     if (vec[0].size() != vec2.size()){
+    //          cout << "please enter two matrices with the compatible dimentions, The number of columns in A should equal the number of rows of B" << endl;
+    //     return 1 ;
+    //     }
+
+    //     mul(vec,vec2);
+    // }
 
 
 
