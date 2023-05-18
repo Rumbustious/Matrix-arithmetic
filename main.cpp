3320490bc16f2bee7163258e400d8349f5dd5e86
Rumbustious <99288580+Rumbustious@users.noreply.github.com>
2023-05-18 08:00:36 +0300

    Edited main     -fixed styling     -added createMatrices function

--- main.cpp
+++ main.cpp
@@ -7,12 +7,9 @@
 
 using namespace std;
 
-
-
-int main(){
-
+void createMatrices(vector<vector<int> > &vec, vector<vector<int> > &vec2) {
     int rows, cols, num;
-    char operation;
+   
     cout << "Enter the number of rows in the first matrix:\n";
     cin >> rows; 
     cout << "Enter the number of columns in the first matrix:\n";
@@ -20,7 +17,6 @@ int main(){
 
     // Initializing the vector of vectors
     vector<vector<int> > vec;
-    vector<vector<int> > vec2;
 
    for (int i = 0; i < rows; i++) {
         // Vector to store column elements
@@ -35,57 +31,99 @@ int main(){
         vec.push_back(v1);
     }
 
+    if (vec2.size() == 1){
+        vector<vector<int> > vec2;
+        cout << "Enter the number of rows in the second matrix:\n";
+        cin >> rows; 
+        cout << "Enter the number of columns in the second matrix:\n";
+        cin >> cols;
 
-    cout << "Enter the number of rows in the second matrix:\n";
+        for (int i = 0; i < rows; i++) {
+            // Vector to store column elements
+            vector<int> v2;
+    
+            for (int j = 0; j < cols; j++) {
+                cin >> num;
+                v2.push_back(num);
+            }
+            // Pushing back above 1D vector
+            // to create the 2D vector
+            vec2.push_back(v2);
+        }
+    }
+}
+void createMatrices(vector<vector<int> > &vec) {
+    int rows, cols, num;
+   
+    cout << "Enter the number of rows in the matrix:\n";
     cin >> rows; 
-    cout << "Enter the number of columns in the second matrix:\n";
+    cout << "Enter the number of columns in the matrix:\n";
     cin >> cols;
 
-     for (int i = 0; i < rows; i++) {
+    // Initializing the vector of vectors
+    vector<vector<int> > vec;
+
+   for (int i = 0; i < rows; i++) {
         // Vector to store column elements
-        vector<int> v2;
+        vector<int> v1;
   
         for (int j = 0; j < cols; j++) {
             cin >> num;
-            v2.push_back(num);
+            v1.push_back(num);
         }
         // Pushing back above 1D vector
         // to create the 2D vector
-        vec2.push_back(v2);
+        vec.push_back(v1);
     }
+}
 
+
+int main(){
+
+    vector<vector<int> > vec;
+    vector<vector<int> > vec2;
 
     // choose the operation
-
-    cout << "To choose the operation enter : \n A for addition \n S for substraction \n M for multiblication \n I for inverse"<< endl;
+    char operation;
+    cout << "Pick up an operation : \nA for Addition of Two Matrices \nS for Subtraction of Two Matrices \nM for Multiplication of Two Matrices\nC for Cofactor of Matrix \nT for Transpose of Matrix \nI for Inverse of Two Matrices"<< endl;
     cin >> operation;
-    if (operation == 'A'){
-
-        if ((vec.size() != vec2.size()) || (vec[0].size() != vec2[0].size())){
-             cout << "please enter two matrices with the same dimentions, Ex: two 4x4 matrices" << endl;
+    if (operation == 'A' || 'a'){
+        createMatrices(vec, vec2);
+         if ((vec.size() != vec2.size()) || (vec[0].size() != vec2[0].size())){
+             cout << "Two matrices should have same dimensions, Ex: two 4x4 matrices" << endl;
             return 1;
         }
-
         add(vec,vec2);
-    }
-    else if (operation == 'S'){
-
+    } else if (operation == 'S' || operation == 's'){
+        createMatrices(vec, vec2);
         if ((vec.size() != vec2.size()) || (vec[0].size() != vec2[0].size())){
-             cout << "please enter two matrices with the same dimentions, Ex: two 4x4 matrices" << endl;
+             cout << "Two matrices should have same dimensions, Ex: two 4x4 matrices" << endl;
             return 1;
         }
 
         subtract(vec,vec2);
-    }
-    else if (operation == 'M'){
-
+    } else if (operation == 'M' || operation == 'm'){
+        createMatrices(vec, vec2);
         if (vec[0].size() != vec2.size()){
-             cout << "please enter two matrices with the compatible dimentions, The number of columns in A should equal the number of rows of B" << endl;
+             cout << "Two matrices should have compatible dimensions (The number of columns in A should equal the number of rows of B)" << endl;
         return 1 ;
         }
-        // 2x3 . 3xY
+      
         multiply(vec,vec2);
+    } else if (operation == 'C' || operation == 'c'){
+        createMatrices(vec);
+        if (vec.size() != vec[0].size()){
+             cout << "Matrix should be a square matrix, Ex: 4x4 matrix" << endl;
+            return 1;
+        }
+        kg
     }
+    
+
+
+
+
+
 
 
 
